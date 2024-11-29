fea133468c3c2a39f8093294857c718425245e6b
afamiliarquiet <afamiliarquiet@gmail.com>
2024-11-28 17:37:58 -0800

    fix youth + mod icon

--- src/main/java/net/hexvolt/pocketwatchery/Pocketwatchery.java
+++ src/main/java/net/hexvolt/pocketwatchery/Pocketwatchery.java
@@ -1,29 +1,32 @@
 package net.hexvolt.pocketwatchery;
 
 import com.mojang.logging.LogUtils;
+import com.mojang.serialization.Codec;
 import net.hexvolt.pocketwatchery.block.ModBlocks;
 import net.hexvolt.pocketwatchery.item.ModItems;
 import net.hexvolt.pocketwatchery.item.custom.PocketwatchBaseItem;
 import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import org.slf4j.Logger;
+
+import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(Pocketwatchery.MODID)
@@ -32,6 +35,11 @@ public class Pocketwatchery {
     public static final String MODID = "pocketwatchery";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, MODID);
+    public static final Supplier<AttachmentType<Integer>> ETERNALLY_YOUNG = ATTACHMENT_TYPES.register(
+            "eternally_young", () -> AttachmentType.builder(() -> -1).serialize(Codec.INT).build()
+    );
 
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
@@ -45,6 +53,7 @@ public class Pocketwatchery {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ATTACHMENT_TYPES.register(modEventBus);
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
         ModSounds.register(modEventBus);
@@ -53,19 +62,19 @@ public class Pocketwatchery {
         modEventBus.addListener(this::addCreative);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+//        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+//        LOGGER.info("HELLO FROM COMMON SETUP");
+//
+//        if (Config.logDirtBlock)
+//            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+//
+//        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+//
+//        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 
     // Add the example block item to the building blocks tab
@@ -92,7 +101,7 @@ public class Pocketwatchery {
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        //LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
--- src/main/java/net/hexvolt/pocketwatchery/mixin/EternalYouthMixin.java
+++ src/main/java/net/hexvolt/pocketwatchery/mixin/EternalYouthMixin.java
@@ -1,5 +1,6 @@
 package net.hexvolt.pocketwatchery.mixin;
 
+import net.hexvolt.pocketwatchery.Pocketwatchery;
 import net.hexvolt.pocketwatchery.item.ModItems;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -9,31 +10,27 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AgeableMob.class)
 public abstract class EternalYouthMixin extends PathfinderMob {
-    @Unique
-    private boolean pocketwatchery$eternallyYoung = false;
+    @Shadow
+    protected int age;
 
     protected EternalYouthMixin(EntityType<? extends PathfinderMob> entityType, Level level) {
         super(entityType, level);
     }
 
-    @Shadow public abstract void setAge(int age);
-
     @Inject(at = @At("HEAD"), method = "setAge", cancellable = true)
     // Prevent young mobs from aging if they been young pocketwatched
     private void setAge(int age, CallbackInfo ci) {
-        if (pocketwatchery$eternallyYoung) {
+        if (this.hasData(Pocketwatchery.ETERNALLY_YOUNG)) {
+            this.age = this.getData(Pocketwatchery.ETERNALLY_YOUNG);
             ci.cancel();
         }
     }
@@ -43,7 +40,7 @@ public abstract class EternalYouthMixin extends PathfinderMob {
         ItemStack itemstack = player.getItemInHand(hand);
         if (itemstack.is(ModItems.POCKETWATCH_YOUTH) && this.isBaby()) {
             player.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
-            this.pocketwatchery$eternallyYoung = true;
+            this.setData(Pocketwatchery.ETERNALLY_YOUNG, this.age);
             return InteractionResult.sidedSuccess(this.level().isClientSide);
         } else {
             return super.mobInteract(player, hand);
